875b3d6d10541961c9bdad83a8fffd59bd7b8109
ARAVINTH0022 <113502178+ARAVINTH0022@users.noreply.github.com>
2026-08-10 00:53:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function validEmail(str) {
         return false;
     }
 
-    const emailRegex = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*\.[a-zA-Z]{2,3}$/;
+    const emailRegex = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*(\.[a-zA-Z]{2,3})+$/;
 
     return emailRegex.test(str);
 }
